5d8c55c8d3b3c5b0d5bbc72cbcc49d01d475eb06
Jorin Eggers <jorineggers@gmail.com>
2024-05-20 09:50:55 +0200

    change settings name add auto save setting add instructions to run code add instructions to comment code

--- turtle.js
+++ turtle.js
@@ -1,1 +1,5 @@
+// Um dein Turtle-Programm zu starten, klicke unten rechts auf "Go Live".
+// Klicke dann im Popup auf "Open in Browser".
+// Am besten platzierst du das Browserfenster so neben deinen Code, dass du beides sehen kannst.
+
 forward(100)
--- variablen.js
+++ variablen.js
@@ -1,6 +1,11 @@
 console.clear()
 
+// In visual studio code kann man mit der Taskenkomibnation Command + Shift + 7
+// mehrere Zeilen auf einmal aus- oder einkommentieren.
+// Markiere dazu die Zeilen, die du auskommentieren möchtest und drücke die Tastenkombination.
 
+
+///////////////////////////////////////////////////////////
 // Aufgabe 1:
 // Ändere die Variablen, sodass sie für dich stimmen.
 var name = "dein Name"
@@ -10,35 +15,43 @@ var ichHabeEinHaustier = false
 console.log("Hallo ich heiße " + name + ".")
 console.log("Ich bin " + alter + " Jahre alt.")
 console.log("Ich habe ein Haustier: " + ichHabeEinHaustier)
+///////////////////////////////////////////////////////////
 
 
 
+///////////////////////////////////////////////////////////////////////////////////
 // Aufgabe 2:
 // Füge fünf weitere Eigenschaft oben hinzu und gebe sie auch über die Konsole aus.
 // z.B. anzahlGeschwister, lieblingsfarbe, ...
+///////////////////////////////////////////////////////////////////////////////////
 
 
 
+///////////////////////////////////////////////////////////////
 // Aufgabe 3:
 // Kannst du ein Popup-Fenster öffnen, in dem du begrüßt wirst?
 // Zum Beispiel: Hallo _____, schön, dass du da bist!
+///////////////////////////////////////////////////////////////
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////
 // Aufgabe 4:
 // Du kannst in einem String eine neue Zeile angfangen, indem du den code \n benutzt.
 // Probiere es aus.
 // var begrüßung = "Hallo " + name + ",\nSchön, dass du da bist!"
+/////////////////////////////////////////////////////////////////////////////////////
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////
 // BONUSAUFGABE:
 // Es ist ganz logisch, dass zwei Zahlen addiert werden, wenn man ein + Zeichen benutzt. Z.B.:
 // var zahl1 = 5
 // var zahl2 = 10
 // console.log(zahl1+zahl2)
-
+// 
 // aber was passiert, wenn man eine Zahl plus einen String rechnet?
-// Was ist mit zwei Strings?
-// Wie ist es bei einem Boolean und einer Zahl?
+// Was ist mit String + String oder Boolean + Zahl?
 // Probiere alle möglichen Kombinationen aus!
+/////////////////////////////////////////////////////////////////////////////////////
